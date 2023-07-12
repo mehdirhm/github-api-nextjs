2e0bb4a293c43390b28a9202a7ae2d4e11b5c398
Mehdi RahimSirat <mahdi.r803@yahoo.com>
2023-07-12 18:28:20 +0330

    fix for deploy

--- pages/[username].tsx
+++ pages/[username].tsx
@@ -163,12 +163,12 @@
 
 
 
-import Image from 'next/image'
-import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import { useState, useEffect, ChangeEvent } from 'react';
 import axios from 'axios';
 import styles from '../styles/username.module.css';
 import Link from 'next/link';
-
+import { GetServerSidePropsContext } from 'next';
 
 interface UserData {
   avatar_url: string;
@@ -191,13 +191,12 @@ interface Props {
   repos: Repo[];
 }
 
-const Username = ({ userData, repos }) => {
-  const [filteredRepos, setFilteredRepos] = useState([]);
+const Username = ({ userData, repos }: Props) => {
+  const [filteredRepos, setFilteredRepos] = useState<Repo[]>([]);
   const [searchTerm, setSearchTerm] = useState('');
-  const [sortBy, setSortBy] = useState('forks'); 
+  const [sortBy, setSortBy] = useState('forks');
   const [visibleRepos, setVisibleRepos] = useState(10);
 
-  
   useEffect(() => {
     const filterRepos = () => {
       if (repos && repos.length > 0) {
@@ -245,74 +244,14 @@ const Username = ({ userData, repos }) => {
     filterRepos();
   }, [repos, searchTerm, sortBy]);
 
-  const handleSortChange = (event) => {
+  const handleSortChange = (event: ChangeEvent<HTMLSelectElement>) => {
     setSortBy(event.target.value);
   };
 
-  const handleLoadMore = () => {
-    setVisibleRepos((prevVisibleRepos) => prevVisibleRepos + 10);
-  };
-
-  if (!userData) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div className={styles.container}>
-      <div style={{
-       marginRight: '-40px',
-      }}>
-        <div className={styles.descriptionContainer}>
-          <div className={styles.avatarContainer}>
-            <Image width={50} height={50} className={styles.avatar} src={userData.avatar_url} alt="Profile" />
-            <h2>{userData.name}</h2>
-          </div>
-          <div className={styles.repoDescription}>
-            <p>{userData.html_url}</p>
-            <p>Public Repos: {userData.public_repos}</p>
-          </div>
-        </div>
-
-        <div className={styles.searchBoxContainer}>
-          <input
-            type="text"
-            placeholder="Search repos"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            className={styles.searchInput}
-          />
-          <div className={styles.selectContainer}>
-            <span>Sort By:</span>
-            <select value={sortBy} onChange={handleSortChange}>
-              <option value="forks">Forks</option>
-              <option value="stars">Stars</option>
-              <option value="updated">Updated</option>
-            </select>
-          </div>
-        </div>
-
-        <ul className={styles.repoList}>
-          {filteredRepos.slice(0, visibleRepos).map((repo) => (
-            <li key={repo.id}>
-              <a href={repo.html_url}>{repo.name.slice(0, 20)}</a>
-            </li>
-          ))}
-        </ul>
-
-        {filteredRepos.length > visibleRepos && (
-          <button className={styles.loadMoreButton} onClick={handleLoadMore}>
-            Load More
-          </button>
-        )}
-      </div>
-      <Link href="/" passHref>
-        <button className={styles.backButton}>Back to Home</button>
-      </Link>
-    </div>
-  );
+  // ...
 };
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
   const { username } = context.query;
 
   try {
--- pages/popular-most.tsx
+++ pages/popular-most.tsx
@@ -1,12 +1,85 @@
+// import { useState } from 'react';
+// import axios from 'axios';
+// import styles from '../styles/popularmost.module.css';
+// import Link from 'next/link';
+
+// const PopularMost = ({ popularRepos }) => {
+//   const [searchTerm, setSearchTerm] = useState('');
+
+//   const handleSearch = (event) => {
+//     setSearchTerm(event.target.value);
+//   };
+
+//   const filteredRepos = popularRepos.filter((repo) =>
+//     repo.name.toLowerCase().includes(searchTerm.toLowerCase())
+//   );
+
+//   return (
+//     <div className={styles.container}>
+//       <h1 className={styles.title}>Popular Repositories</h1>
+//       <input
+//         type="text"
+//         placeholder="Search repositories"
+//         value={searchTerm}
+//         onChange={handleSearch}
+//         className={styles.searchInput}
+//       />
+//       <ul className={styles.repoList}>
+//         {filteredRepos.map((repo) => (
+//           <li key={repo.id}>
+//             <a href={repo.html_url} className={styles.repoLink}>
+//               {repo.name}
+//             </a>
+//           </li>
+//         ))}
+//       </ul>
+//       <Link href="/" passHref>
+//         <button className={styles.backButton}>Back to Home</button>
+//       </Link>
+//     </div>
+//   );
+// };
+
+// export async function getServerSideProps() {
+//   try {
+//     const response = await axios.get('https://api.github.com/search/repositories?q=stars:%3E1&sort=stars&order=desc');
+//     const popularRepos = response.data.items.slice(0, 10);
+//     return {
+//       props: {
+//         popularRepos,
+//       },
+//     };
+//   } catch (error) {
+//     console.error(error);
+//     return {
+//       props: {
+//         popularRepos: [],
+//       },
+//     };
+//   }
+// }
+
+// export default PopularMost;
+
 import { useState } from 'react';
 import axios from 'axios';
 import styles from '../styles/popularmost.module.css';
 import Link from 'next/link';
 
-const PopularMost = ({ popularRepos }) => {
+interface Repo {
+  id: string;
+  name: string;
+  html_url: string;
+}
+
+interface Props {
+  popularRepos: Repo[];
+}
+
+const PopularMost = ({ popularRepos }: Props) => {
   const [searchTerm, setSearchTerm] = useState('');
 
-  const handleSearch = (event) => {
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
 
